ece0fc776ee044ac0640f050ee401e402c962bad
Aadesh Shrestha <aadeshere1@gmail.com>
2023-03-16 17:53:22 +0545

    abc

--- template.rb
+++ template.rb
@@ -1,6 +1,32 @@
 require "fileutils"
 require "shellwords"
 
+
+
+# Copied from: https://github.com/mattbrictson/rails-template
+# Add this template directory to source_paths so that Thor actions like
+# copy_file and template resolve against our source files. If this file was
+# invoked remotely via HTTP, that means the files are not present locally.
+# In that case, use `git clone` to download them to a local temporary dir.
+def add_template_repository_to_source_path
+  if __FILE__ =~ %r{\Ahttps?://}
+    require "tmpdir"
+    source_paths.unshift(tempdir = Dir.mktmpdir("jumpy-"))
+    at_exit { FileUtils.remove_entry(tempdir) }
+    git clone: [
+      "--quiet",
+      "https://github.com/aadeshere1/jumpy.git",
+      tempdir
+    ].map(&:shellescape).join(" ")
+
+    if (branch = __FILE__[%r{jumpy/(.+)/template.rb}, 1])
+      Dir.chdir(tempdir) { git checkout: branch }
+    end
+  else
+    source_paths.unshift(File.dirname(__FILE__))
+  end
+end
+
 def rails_version
   @rails_version ||= Gem::Version.new(Rails::VERSION::STRING)
 end
@@ -91,7 +117,7 @@ end
 # 
 
 unless rails_7_or_newer?
-  puts "Please update Rails to 7.0.4 or newer to create a application through Jumpstart"
+  puts "Please update Rails to 7.0.4 or newer to create a application through jumpy"
 end
 
 add_gems
@@ -121,7 +147,7 @@ after_bundle do
   end
   
   say
-  say "Jumpstart app successfully created!", :blue
+  say "jumpy app successfully created!", :blue
   say
   say "To get started with your new app:", :green
   say "  cd #{original_app_name}"
