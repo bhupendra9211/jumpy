3df0b1798149c5e8d963eddc04d24518c5891ff9
bhupendra9211 <shahbhupendra9211@gmail.com>
2024-05-30 15:51:15 +0545

    adding contact model and static_pages for terms and privacy

--- template.rb
+++ template.rb
@@ -426,6 +426,80 @@ after_bundle do
   generate 'paper_trail:install'
   rails_command 'db:migrate'
 
+  generate "model Contact email:string content:text"
+  generate "controller Static_Pages terms privacy_policy"
+
+  create_file "app/controllers/contacts_controller.rb", <<-CODE
+    class ContactsController < ApplicationController
+      def new
+        @contact = Contact.new
+      end
+
+      def create
+        @contact = Contact.new(contact_params)
+        if @contact.save
+          redirect_to new_contact_path, notice: "Message sent successfully"
+        else
+          render :new
+        end
+      end
+
+      private
+
+      def contact_params
+        params.require(:contact).permit(:email, :content)
+      end
+    end
+  CODE
+
+  route <<-CODE
+    resources :contacts, only: [:new, :create]
+    get 'terms', to: 'static_pages#terms'
+    get 'privacy_policy', to: 'static_pages#privacy_policy'
+  CODE
+
+  create_file "app/views/contacts/new.html.erb", <<-CODE
+    <section class="bg-white dark:bg-gray-700">
+      <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+        <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact Us</h2>
+
+        <%= form_with(model: @contact, local: true) do |form| %>
+          <div class="space-y-8">
+            <div>
+              <%= form.label :email, class: "block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300" %>
+              <%= form.text_field :email, class: "shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light", placeholder: "name@example.com", required: true %>
+            </div>
+
+            <div class="sm:col-span-2">
+              <%= form.label :content, class: "block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400" %>
+              <%= form.text_area :content, rows: "6", class: "block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500", placeholder: "Leave a comment..." %>
+            </div>
+
+            <div class="flex justify-center">
+              <%= form.submit "Send message", class: "py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800" %>
+            </div>
+          </div>
+        <% end %>
+      </div>
+    </section>
+  CODE
+
+  create_file "app/views/pages/terms.html.erb", <<-CODE
+    <h1>Terms and Conditions</h1>
+    <p>Insert your terms and conditions here.</p>
+  CODE
+
+  create_file "app/views/pages/privacy_policy.html.erb", <<-CODE
+    <h1>Terms and Conditions</h1>
+    <p>Insert your terms and conditions here.</p>
+  CODE
+
+  create_file "app/admin/contacts.rb", <<-CODE
+    ActiveAdmin.register Contact do
+      permit_params :email, :content
+    end
+  CODE
+
   
 
   run "cp config/environments/production.rb config/environments/staging.rb"
